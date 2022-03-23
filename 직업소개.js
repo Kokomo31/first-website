58ebe3ba80319b883df71d641d81de9c7238dd53
Kokomo31 <jdq6105@gmail.com>
2022-03-23 22:48:40 +0900

    use opacity funtcion

--- 직업소개.js
+++ 직업소개.js
@@ -1,6 +1,11 @@
-var 반투명아이콘 = document.querySelector('.Classicon');
+var 클래스아이콘 = document.querySelector('.Classicon');
+var 클래스비디오 = document.querySelector('.동영상보기')
 
 function 반투명화 {
+  if(클래스아이콘.style.onmouseup){
+    클래스아이콘.style.opacity= 0.5;
+    클래스비디오.style.opacity= 1;
+  }
 
-  반투명아이콘.style.opacity = 0.3
+
 }
